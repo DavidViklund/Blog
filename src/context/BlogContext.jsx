5b43971bda0e8a2dc5f5054e2a7d1ddd6b74df92
DavidViklund <david.viklund@chasacademy.se>
2024-05-23 16:16:47 +0200

    sista ändringarna innnan städ

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -9,18 +9,21 @@ export const useBlogContext = () => useContext(BlogContext);
 
 export const BlogProvider = ({ children }) => {
   const { currentUser } = useAuth();
+  
+  const defaultPosts = [
+    {
+      id: 1,
+      title: 'Angående Naturvin',
+      author: 'jane.doe@example.com',
+      content: 'Upptäck Naturvin: En Smakupplevelse från Naturen. Naturvin är mer än bara en dryck – det är en resa tillbaka till naturens ursprung. Tillverkat med minimal mänsklig inblandning, naturvin låter druvorna och jorden tala för sig själva. Utan tillsatser och med naturlig jäsning, erbjuder varje flaska en unik smakprofil. Föreställ dig att sippa på ett vin som doftar av vildblommor, kryddor och frukt, där varje sipp ger en ny överraskning. Naturvin hyllar den råa skönheten i druvorna och terroiren de växer i, vilket skapar en genuin och autentisk vinupplevelse. Att välja naturvin är att välja en livsstil som värderar hållbarhet, autenticitet och en djupare koppling till naturen. Så nästa gång du ska välja vin, prova ett naturvin och låt dig förföras av dess naturliga charm och komplexitet. Skål för naturens egen konst!',
+      category: 'Vin',
+      comments: [],
+    },
+  ];
+  
   const [posts, setPosts] = useState(() => {
     const storedPosts = localStorage.getItem('posts');
-    return storedPosts ? JSON.parse(storedPosts) : [
-      {
-        id: 1,
-        title: 'Angående Naturvin',
-        author: 'jane.doe@example.com',
-        content: 'Upptäck Naturvin...',
-        category: 'Vin',
-        comments: [],
-      },
-    ];
+    return storedPosts ? JSON.parse(storedPosts) : defaultPosts;
   });
 
   useEffect(() => {
